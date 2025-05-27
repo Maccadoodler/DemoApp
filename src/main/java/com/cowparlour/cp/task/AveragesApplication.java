23c9b1cd37ad676eabd2b5988cbf36ce34b320d2
Your Name <you@example.com>
2025-05-27 18:49:54 +0100

    Initializr

---
+++ src/main/java/com/cowparlour/cp/task/AveragesApplication.java
@@ -0,0 +1,13 @@
+package com.cowparlour.cp.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AveragesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AveragesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cowparlour/cp/task/AveragesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cowparlour.cp.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AveragesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
