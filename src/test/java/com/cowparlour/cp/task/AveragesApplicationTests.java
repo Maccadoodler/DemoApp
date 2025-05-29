9357830f10f4615856bfa492cdfd90cdab546a3f
Your Name <you@example.com>
2025-05-29 18:32:53 +0100

    Added Some unit tests with real database

--- src/test/java/com/cowparlour/cp/task/AveragesApplicationTests.java
+++ src/test/java/com/cowparlour/cp/task/AveragesApplicationTests.java
@@ -1,13 +1,72 @@
+/*
+ * (C): cowparlour.com  2025
+ */
 package com.cowparlour.cp.task;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@SpringBootTest
+import com.cowparlour.cp.task.dto.TaskTime;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.math.BigInteger;
+
+
+/**
+ * Unit test to run against a running system with database.
+ */
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 class AveragesApplicationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void testAverage_NoRecord() throws Exception {
+		mockMvc.perform(get("/time/average/John"))
+				.andExpect(status().isNotFound());
+	}
+
+	@Test
+	void testAverage_TwoRecord() throws Exception {
+
+		TaskTime input = new TaskTime("jmc", BigInteger.TEN);
+		TaskTime inputTwo = new TaskTime("jmc", BigInteger.valueOf(20L));
+
+		ObjectMapper objectMapper = new ObjectMapper();
+
+		String json = objectMapper.writeValueAsString(input);
+		String jsonTwo = objectMapper.writeValueAsString(inputTwo);
+
+		mockMvc.perform(post("/time/record")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(json))
+				.andExpect(status().isOk())
+				.andExpect(content().string("Task jmc processed successfully."));
+
+		mockMvc.perform(post("/time/record")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(jsonTwo))
+				.andExpect(status().isOk())
+				.andExpect(content().string("Task jmc processed successfully."));
+
+		mockMvc.perform(get("/time/average/jmc"))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.task").value("jmc"))
+				.andExpect(jsonPath("$.averageDuration").value(BigInteger.valueOf(15L)));
 	}
 
 }
--- src/test/java/com/cowparlour/cp/task/repository/TaskMetricTest.java
+++ src/test/java/com/cowparlour/cp/task/repository/TaskMetricTest.java
@@ -4,7 +4,6 @@ package com.cowparlour.cp.task.repository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.math.BigInteger;
